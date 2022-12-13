7ab3f4262977d1f2ce24a3fa6df682ef7b54b454
Leah Cardoz <leahcardoz@gmail.com>
2022-12-13 16:55:47 -0500

    commit tree 9dc2b7a9b07eafc403ae9e809fbd338721d26b06

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,9 +1,19 @@
+# `birds controller`
 class BirdsController < ApplicationController
-
   # GET /birds
   def index
     birds = Bird.all
-    render json: birds
+    render json: birds, status: :ok
+  end
+
+  # GET /birds/:id
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: bird, status: :ok
+    else
+      render json: { error: 'Bird not found' }, status: :not_found
+    end
   end
 
   # POST /birds
@@ -12,20 +22,30 @@ class BirdsController < ApplicationController
     render json: bird, status: :created
   end
 
-  # GET /birds/:id
-  def show
-    bird = Bird.find_by(id: params[:id])
+  # PATCH (update) /birds/:id
+  def update
+    bird = Bird.find(params[:id])
     if bird
-      render json: bird
+      bird.update(bird_params)
+      render json: bird, status: :accepted
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      render json: { error: 'Bird not found' }, status: :not_found
+    end
+  end
+
+  def increment_likes
+    bird = Bird.find(params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird, status: :accepted
+    else
+      render json: { error: 'Bird not found' }, status: :not_found
     end
   end
 
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
-
 end
